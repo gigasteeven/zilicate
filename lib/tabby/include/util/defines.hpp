0bcdc2eb422dfd227814a2a57fbd60dc973da652
gigasteeven <kaiserwilgeim6@gmail.com>
2026-06-27 04:52:21 +0500

    we gone

--- lib/tabby/include/util/defines.hpp
+++
@@ -1,8 +0,0 @@
-#pragma once
-#ifndef TABBY_UTIL_DEFINES
-#define TABBY_UTIL_DEFINES
-
-#define TABBY_NS_BEGIN namespace tabby {
-#define TABBY_NS_END } // tabby
-
-#endif // TABBY_UTIL_DEFINES
--- src/main.cpp
+++
@@ -1,9 +0,0 @@
-#include <Geode/Geode.hpp>
-#include <Geode/modify/CCScheduler.hpp>
-
-using namespace geode::prelude;
-
-#include "bot/bot.hpp"
-// #include "recorder/recorder.hpp"
-
-$on_mod(Loaded) { Bot::get()->initialize(); }
